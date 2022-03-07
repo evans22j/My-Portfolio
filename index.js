876faf650bc38a2bc1b315fdbd09bf8e4f6d73a4
Evans Sitibekiso <jevacrism@gmail.com>
2022-03-07 16:40:36 +0200

    changes on index.js,css and html

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const mainMenu= document.querySelector(".menu");
+const mainMenu = document.querySelector(".menu");
+const nav = document.querySelector('.menu-mobile')
 const navList = document.querySelector(".nav-list");
+const bars = document.getElementById('bars')
+const xBtn = document.querySelector('.x-btn');
+const links = document.querySelectorAll('.link')
 
-menu.addEventListener("click",() => {
-    menu.classList.toggle("active");
-    nav-list.classList.toggle("active");
+
+function pop() {
+    nav.classList.add('popNav')
+    navList.classList.add('popUl')
+
+}
+
+bars.addEventListener("click",pop)
+
+function closePopup(){
+    nav.classList.remove('popNav')
+    navList.classList.remove('popUl')
+}
+xBtn.addEventListener("click",closePopup)
+
+links.forEach((link) =>{
+    link.addEventListener("click",closePopup)
 })
