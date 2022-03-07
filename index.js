0f23f77cb441d0109c9ef64737643cf0aa5a72b8
Evans Sitibekiso <jevacrism@gmail.com>
2022-03-07 12:46:34 +0200

    Add menu and nav-list on index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const mainMenu= document.querySelector(".menu");
+const navList = document.querySelector(".nav-list");
 
+menu.addEventListener("click",() => {
+    menu.classList.toggle("active");
+    nav-list.classList.toggle("active");
+})
