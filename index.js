970d258981d6bd97feba1d8b0464c4b69d17eb48
Evans Sitibekiso <jevacrism@gmail.com>
2022-03-11 00:27:16 +0200

    changes on html,css and javascipt

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const pop = () => {
-  const nav = document.querySelector('.menu-mobile');
-  const navList = document.querySelector('.nav-list');
-  const blur = document.getElementById('header-text');
-  nav.classList.add('popNav');
-  document.body.classList.add('hidden');
-  navList.classList.add('popUl');
-  blur.style.filter = 'blur(5px)';
+  const nav = document.querySelector(".menu-mobile");
+  const navList = document.querySelector(".nav-list");
+  const blur = document.getElementById("header-text");
+  nav.classList.add("popNav");
+  document.body.classList.add("hidden");
+  navList.classList.add("popUl");
+  blur.style.filter = "blur(5px)";
 };
 
 const closePopup = () => {
-  const nav = document.querySelector('.menu-mobile');
-  const navList = document.querySelector('.nav-list');
-  const blur = document.getElementById('header-text');
-  nav.classList.remove('popNav');
-  navList.classList.remove('popUl');
-  document.body.classList.remove('hidden');
-
-  blur.style.filter = 'none';
+  const nav = document.querySelector(".menu-mobile");
+  const navList = document.querySelector(".nav-list");
+  const blur = document.getElementById("header-text");
+  nav.classList.remove("popNav");
+  navList.classList.remove("popUl");
+  document.body.classList.remove("hidden");
+  blur.style.filter = "none";
 };
 
-const bars = document.getElementById('bars');
-bars.addEventListener('click', pop);
+const bars = document.getElementById("bars");
+bars.addEventListener("click", pop);
 
-const evans = document.querySelector('.evans');
-evans.addEventListener('click', closePopup);
+const evans = document.querySelector(".evans");
+evans.addEventListener("click", closePopup);
 
-const links = document.querySelectorAll('.link');
+const links = document.querySelectorAll(".link");
 links.forEach((link) => {
-  link.addEventListener('click', closePopup);
+  link.addEventListener("click", closePopup);
 });
 
 
+const seeProject = document.querySelectorAll('.btn-pop');
+const popup = document.querySelector('.pop-bg')
+const closeBtn = document.querySelector('.close-x')
 
-// POPUP WINDOW
+seeProject.forEach(project => {
+  project.addEventListener('click',() => {
+    popup.classList.add('display')
+  })
+})
+
