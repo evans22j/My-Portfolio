cf3143316aeb9c0aaf1122fa5c7aa70442c2b905
Evans Sitibekiso <jevacrism@gmail.com>
2022-03-11 21:16:28 +0200

    changes on index.js

--- index.js
+++ index.js
@@ -1,86 +1,88 @@
 const pop = () => {
-  const nav = document.querySelector('.menu-mobile');
-  const navList = document.querySelector('.nav-list');
-  const blur = document.getElementById('header-text');
-  nav.classList.add('popNav');
-  document.body.classList.add('hidden');
-  navList.classList.add('popUl');
-  blur.style.filter = 'blur(5px)';
+  const nav = document.querySelector(".menu-mobile");
+  const navList = document.querySelector(".nav-list");
+  const blur = document.getElementById("header-text");
+  nav.classList.add("popNav");
+  document.body.classList.add("hidden");
+  navList.classList.add("popUl");
+  blur.style.filter = "blur(5px)";
 };
 
 const closePopup = () => {
-  const nav = document.querySelector('.menu-mobile');
-  const navList = document.querySelector('.nav-list');
-  const blur = document.getElementById('header-text');
-  nav.classList.remove('popNav');
-  navList.classList.remove('popUl');
-  document.body.classList.remove('hidden');
-  blur.style.filter = 'none';
+  const nav = document.querySelector(".menu-mobile");
+  const navList = document.querySelector(".nav-list");
+  const blur = document.getElementById("header-text");
+  nav.classList.remove("popNav");
+  navList.classList.remove("popUl");
+  document.body.classList.remove("hidden");
+  blur.style.filter = "none";
 };
 
-const bars = document.getElementById('bars');
-bars.addEventListener('click', pop);
+const bars = document.getElementById("bars");
+bars.addEventListener("click", pop);
 
-const evans = document.querySelector('.evans');
-evans.addEventListener('click', closePopup);
+const evans = document.querySelector(".evans");
+evans.addEventListener("click", closePopup);
 
-const links = document.querySelectorAll('.link');
+const links = document.querySelectorAll(".link");
 links.forEach((link) => {
-  link.addEventListener('click', closePopup);
+  link.addEventListener("click", closePopup);
 });
 
-const seeProject = document.querySelectorAll('.btn-pop');
-const popup = document.querySelector('.pop-bg');
+// pop up window
+
+const seeProject = document.querySelectorAll(".btn-pop");
+const popup = document.querySelector(".pop-bg");
 
 seeProject.forEach((project) => {
-  project.addEventListener('click', () => {
-    popup.classList.add('display');
+  project.addEventListener("click", () => {
+    popup.classList.add("display");
   });
 });
 
-const ul = document.querySelector('.cards');
-const popDiv = document.querySelector('.pop-bg');
+const ul = document.querySelector(".cards");
+const popDiv = document.querySelector(".pop-bg");
 
 const cards = [
   {
     id: 1,
-    title: 'Tonic',
-    image: './photos/Portfolio_1.png',
-    detail: ['Canopy', 'Back End Dev', '2015'],
+    title: "Tonic",
+    image: "./photos/Portfolio_1.png",
+    detail: ["Canopy", "Back End Dev", "2015"],
     description: `A daily selection of privately personalized reads no accounts or
      sign-ups required`,
-    language: ['html', 'css', 'JavaScript'],
-    links: ['https://www.google.com', 'https://www.google.com'],
+    language: ["html", "css", "JavaScript"],
+    links: ["https://www.google.com", "https://www.google.com"],
   },
   {
     id: 2,
-    title: 'Multi-Post Stories',
-    image: './photos/Portfolio2.png',
-    detail: ['Canopy', 'Back End Dev', '2015'],
+    title: "Multi-Post Stories",
+    image: "./photos/Portfolio2.png",
+    detail: ["Canopy", "Back End Dev", "2015"],
     description: `A daily selection of privately personalized reads no accounts or
      sign-ups required`,
-    language: ['html', 'css', 'JavaScript'],
-    links: ['https://www.google.com', 'https://www.google.com'],
+    language: ["html", "css", "JavaScript"],
+    links: ["https://www.google.com", "https://www.google.com"],
   },
   {
     id: 3,
-    title: 'Tonic',
-    image: './photos/Portfolio3.png',
-    detail: ['Canopy', 'Back End Dev', '2015'],
+    title: "Tonic",
+    image: "./photos/Portfolio3.png",
+    detail: ["Canopy", "Back End Dev", "2015"],
     description: `A daily selection of privately personalized reads no accounts or
      sign-ups required`,
-    language: ['html', 'css', 'JavaScript'],
-    links: ['https://www.google.com', 'https://www.google.com'],
+    language: ["html", "css", "JavaScript"],
+    links: ["https://www.google.com", "https://www.google.com"],
   },
   {
     id: 4,
-    title: 'Multi-Post Stories',
-    image: './photos/Portfolio2.png',
-    detail: ['Canopy', 'Back End Dev', '2015'],
+    title: "Multi-Post Stories",
+    image: "./photos/Portfolio2.png",
+    detail: ["Canopy", "Back End Dev", "2015"],
     description: `A daily selection of privately personalized reads no accounts or
      sign-ups required`,
-    language: ['html', 'css', 'JavaScript'],
-    links: ['https://www.google.com', 'https://www.google.com'],
+    language: ["html", "css", "JavaScript"],
+    links: ["https://www.google.com", "https://www.google.com"],
   },
 ];
 
@@ -94,7 +96,7 @@ const projecCards = cards
             <div class="text-div">
               <h2 class="title">${card.title}</h2>
               <ul class="tech-btns">
-              ${card.detail.map((d) => `<li class="common">${d}</li>`).join('')}
+              ${card.detail.map((d) => `<li class="common">${d}</li>`).join("")}
             
               </ul>
 
@@ -102,21 +104,21 @@ const projecCards = cards
                 ${card.description}
               </p>
               <ul class="languages">
-                ${card.language.map((lang) => `<li>${lang}</li>`).join('')}
+                ${card.language.map((lang) => `<li>${lang}</li>`).join("")}
               </ul>
               <button class="see-project btn-pop" data-index-number="2" href="#"
                onclick=(showpopup(${card.id})) >See Project</button
               >
             </div>
           </li>
-`,
+`
   )
-  .join('');
+  .join("");
 
 ul.innerHTML += projecCards;
 
 const modalData = (card) => {
-  const template = document.createElement('template');
+  const template = document.createElement("template");
 
   template.innerHTML = `
         <li class="card-details pop-data">
@@ -140,18 +142,18 @@ const modalData = (card) => {
             </p>
             <div>
             <ul class="languages">
-            ${card.language.map((lang) => `<li>${lang}</li>`).join('')}
+            ${card.language.map((lang) => `<li>${lang}</li>`).join("")}
             </ul>
             <div class="links">
 
             <button class="see-project see-project-dsk" data-index-number="2" href="${
-  card.links[0]
-}"
+              card.links[0]
+            }"
               >See live <span> <img class="logo-tag" src="./Icon.png" alt="" srcset=""></span> </button
             >
              <button class="see-project see-project-dsk" data-index-number="2" href="${
-  card.links[1]
-}"
+               card.links[1]
+             }"
               >See source <span><img class="logo-tag" src="./Vector.png" alt="" srcset=""></span> 
             </button>
           </div>
@@ -165,16 +167,58 @@ const modalData = (card) => {
   popDiv.appendChild(template.content);
 };
 
-/* eslint-disable */ 
-const seeProjects = document.querySelectorAll('.see-project');
+/* eslint-disable */
+const seeProjects = document.querySelectorAll(".see-project");
 function showpopup(id) {
   cards.find((item) => {
     if (Number(id) === Number(item.id)) return modalData(item);
   });
-  popDiv.classList.add('display');
+  popDiv.classList.add("display");
 }
 
 function vanish() {
-  popDiv.classList.remove('display');
-  popDiv.innerHTML = '';
+  popDiv.classList.remove("display");
+  popDiv.innerHTML = "";
 }
+
+
+//form validation
+
+// const form = document.getElementById("contact")
+// const email = document.getElementById("email")
+// const textArea = document.getElementById("textarea")
+
+
+// form.addEventListener("submit",e => {
+//   e.preventDefault();
+
+//   validateImputs();
+// })
+
+// const validateImputs = () => {
+
+//   const textAreaValue = textarea.value.trim();
+//   const emailValue = email.value.trim();
+// }
+
+// Form Validation
+
+const errorMessage = document.querySelector('.error-message');
+const form = document.querySelector('.form');
+const formElement = form.querySelectorAll('input, textarea');
+
+formElement.forEach((fe) => {
+  fe.addEventListener('input', () => {
+    errorMessage.style.display = 'none';
+  });
+});
+
+document.getElementById('submit').addEventListener('click', (e) => {
+  const email = document.getElementById('email').value;
+  const emailValidation = /[A-Z]/.test(email);
+
+  if (emailValidation) {
+    errorMessage.style.display = 'block';
+    e.preventDefault();
+  }
+});
